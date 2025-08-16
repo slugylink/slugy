61cd214e5175ffe5110b01040b191d3ebbad1055
sandip_07 <sarkar.sandip5566@gmail.com>
2025-08-16 11:32:08 +0530

    neon db replica

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,9 +1,10 @@
-// lib/db.ts
 import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { neonConfig } from "@neondatabase/serverless";
 import ws from "ws";
+import { readReplicas } from "@prisma/extension-read-replicas";
 
+// Neon WebSocket config for Node.js
 neonConfig.webSocketConstructor = ws;
 neonConfig.poolQueryViaFetch = true;
 
@@ -15,10 +16,17 @@ function createPrismaClient() {
   const adapter = new PrismaNeon({
     connectionString: process.env.DATABASE_URL!,
   });
-  return new PrismaClient({
-    adapter,
-    log: ["error"],
-  });
+  let client = new PrismaClient({ adapter, log: ["error"] });
+
+  // Use the replica if defined
+  if (process.env.DATABASE_REPLICA_URL) {
+    client = client.$extends(
+      readReplicas({
+        url: process.env.DATABASE_REPLICA_URL,
+      }),
+    ) as unknown as PrismaClient;
+  }
+  return client;
 }
 
 export const db = globalThis.prisma ?? createPrismaClient();
