20a954be6e3d93767bf1534396d6c26118682fe8
sandip-dev07 <sarkarsandip7910@gmail.com>
2025-08-23 20:02:00 +0530

    neon replica

--- src/server/neon.ts
+++ src/server/neon.ts
@@ -1,7 +1,9 @@
 import { neon } from "@neondatabase/serverless";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is required");
+if (!process.env.DATABASE_REPLICA_URL) {
+  throw new Error("DATABASE_REPLICA_URL is required");
 }
 
-export const sql = neon(process.env.DATABASE_URL);
+export const sql = neon(
+  process.env.DATABASE_REPLICA_URL || process.env.DATABASE_URL!,
+);
