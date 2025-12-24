fa54af70cb4368149178eba78a369557ff31d82c
slugylink <slug0urls@gmail.com>
2025-12-24 22:53:37 +0530

    app logo

--- src/components/web/app-logo.tsx
+++ src/components/web/app-logo.tsx
@@ -3,13 +3,22 @@ import React from "react";
 
 const AppLogo = () => {
   return (
-    <div className="relative flex h-8 w-8 items-center justify-center overflow-hidden rounded-lg bg-black shadow-sm">
+    <div className="relative flex h-8 w-8 items-center justify-center overflow- rounded-lg bg-black shadow-sm">
       {/* <Link size={16} /> */}
       <Image
         src="/icon.svg"
         alt="Slugy"
         fill
-        className="object-contain p-[2px]"
+        className="object-contain p-[2px] overflow-hidden rounded-2xl"
+        priority
+      />
+
+      <Image
+        src="/icons/cap.png"
+        alt="Slugy"
+        width={35}
+        height={35}
+        className="absolute -top-3.5 -left-3.5 z-10 object-contain p-[2px]"
         priority
       />
     </div>
